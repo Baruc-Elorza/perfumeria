b4e262eb7df1f286481397385853f4bfc5d98548
Baruc <3baruc69@gmail.com>
2026-09-22 18:41:41 -0600

    Agregar backend y frontend

---
+++ Backend/src/main/java/com/example/demo/controller/HolaController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
+public class HolaController {
+    @GetMapping("/hola")
+    public String Hola(){
+        return "Hola desde Spring Boot";
+    }
+}
