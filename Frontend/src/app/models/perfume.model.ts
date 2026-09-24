eb78d7c84c016acd2c8c70753b242b2473308f57
Jose Angel Sebastian <joseangel@gmail.com>
2026-09-24 00:04:57 -0600

    feat(frontend): agregar modelo e interfaz de servicio HTTP para perfumes

---
+++ Frontend/src/app/models/perfume.model.ts
@@ -0,0 +1,8 @@
+export interface Perfume {
+  id?: number;
+  nombre: string;
+  marca: string;
+  precio: number;
+  stock: number;
+  imagenUrl: string;
+}
---
+++ Frontend/src/app/services/perfume.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Perfume } from '../models/perfume.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PerfumeService {
+  private apiUrl = 'http://localhost:8080/api/perfumes';
+
+  constructor(private http: HttpClient) { }
+
+  getPerfumes(): Observable<Perfume[]> {
+    return this.http.get<Perfume[]>(this.apiUrl);
+  }
+
+  agregarPerfume(perfume: Perfume): Observable<Perfume> {
+    return this.http.post<Perfume>(this.apiUrl, perfume);
+  }
+
+  eliminarPerfume(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
